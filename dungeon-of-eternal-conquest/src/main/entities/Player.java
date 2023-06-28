5c2c8f722a242c02ba40e56daf3cf34a0d133bf8
julio <julioigreja.dev@gmail.com>
2023-06-28 19:24:26 -0300

    feat: added sprite for show life the of player

--- dungeon-of-eternal-conquest/src/main/entities/Player.java
+++ dungeon-of-eternal-conquest/src/main/entities/Player.java
@@ -1,15 +1,25 @@
 package main.entities;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import main.Game;
 import main.dungeon.Camera;
 import main.dungeon.Dungeon;
+import main.util.Spritesheet;
 
 public class Player extends Entity {
 
+	private final BufferedImage spriteLife;
+	
 	public Player() throws IOException {
-		super(0, 0, 16, 16, 1, 1, 5, 2, 3, 5, 6, "/sprites/entities/player.png");
+		super(0, 0, 16, 16, 1, 1, 5, 2, 4, 5, 6, "/sprites/entities/player.png");
+
+		Spritesheet spritesheet = new Spritesheet("/sprites/entities/player.png");
+
+		spriteLife = spritesheet.getSprite(0, 64, 80, 16);
 	}
 
 	@Override
@@ -63,6 +73,47 @@ public class Player extends Entity {
 
 		Camera.x = Camera.clamp((int) (x - (Game.WIDTH / 2)), 0, dungeon.WIDTH * 16 - Game.WIDTH);
 		Camera.y = Camera.clamp((int) (y - (Game.HEIGHT / 2)), 0, dungeon.HEIGHT * 16 - Game.HEIGHT);
+	}
+	
+	private void showLife(Graphics graphics) {
+		graphics.setColor(Color.RED);
+		
+		if (life >= 1) {
+			graphics.fillRect(17, 2, 15, 11);
+		}
+		
+		if (life >= 2) {
+			graphics.fillRect(33, 2, 15, 11);
+		}
+		
+		if (life >= 3) {
+			graphics.fillRect(47, 2, 15, 11);
+		}
+		
+		if (life == 4) {
+			graphics.fillRect(63, 2, 15, 11);
+		}
+
+		graphics.drawImage(spriteLife, 0, 0, 80, 16, null);
+	}
+	
+	@Override
+	public void render(Graphics graphics) {
+		if (dir == dirRight) {
+			if (moved) {
+				graphics.drawImage(movingRightEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			} else {
+				graphics.drawImage(idleRightEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			}
+		} else {
+			if (moved) {
+				graphics.drawImage(movingLeftEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			} else {
+				graphics.drawImage(idleLeftEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			}
+		}
+		
+		this.showLife(graphics);
 	}
 
 }
