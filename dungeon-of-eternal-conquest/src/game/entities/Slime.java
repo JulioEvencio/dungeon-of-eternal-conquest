91d999005cb3236a0e33fc44731f447cdf9b79fd
julio <julioigreja.dev@gmail.com>
2023-07-01 14:54:58 -0300

    feat: added field of view for slime

--- dungeon-of-eternal-conquest/src/game/entities/Slime.java
+++ dungeon-of-eternal-conquest/src/game/entities/Slime.java
@@ -1,13 +1,32 @@
 package game.entities;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.io.IOException;
 
+import game.scenarios.Camera;
 import game.scenarios.Dungeon;
 
 public class Slime extends Entity {
+	
+	private double maskVisionX;
+	private double maskVisionY;
+	private int maskVisionW;
+	private int maskVisionH;
 
 	public Slime() throws IOException {
 		super(0, 0, 16, 16, 0.5, 1, 1, 0, 1, 5, 6, "/sprites/entities/slime.png");
+	}
+	
+	protected void updateMaskVision() {
+		this.maskVisionX = x - 60;
+		this.maskVisionY = y - 60;
+		this.maskVisionW = 150;
+		this.maskVisionH = 150;
+	}
+	
+	public Rectangle getRectangleVision() {
+		return new Rectangle((int) maskVisionX, (int) maskVisionY, maskVisionW, maskVisionH);
 	}
 	
 	@Override
@@ -16,10 +35,16 @@ public class Slime extends Entity {
 		this.maskY = y + 5;
 		this.maskW = width;
 		this.maskH = height - 7;
+		
+		this.updateMaskVision();
 	}
 
 	@Override
 	public void tick(Dungeon dungeon) {
+		if (!dungeon.player.getRectangle().intersects(this.getRectangleVision())) {
+			return;
+		}
+		
 		moved = false;
 		
 		if (dungeon.player.isAttacking() && dungeon.player.getMaskAttack().intersects(this.getRectangle())) {
@@ -62,6 +87,24 @@ public class Slime extends Entity {
 				index = 0;
 			}
 		}
+		
+		this.updateMask();
 	}
 
+	public void render(Graphics graphics) {
+		if (dir == dirRight) {
+			if (moved) {
+				graphics.drawImage(movingRightEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			} else {
+				graphics.drawImage(idleRightEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			}
+		} else {
+			if (moved) {
+				graphics.drawImage(movingLeftEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			} else {
+				graphics.drawImage(idleLeftEntity[index], (int) (x - Camera.x), (int) (y - Camera.y), width, height, null);
+			}
+		}
+	}
+	
 }
